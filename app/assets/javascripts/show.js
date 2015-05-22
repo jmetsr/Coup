422f825821e3e030d5749fdd00993c88c36ba750
Jeremy Jaffe <jmetsr@gmail.com>
2015-05-22 13:25:02 -0400

    refactor game show page

---
+++ app/assets/javascripts/show.js
@@ -0,0 +1,62 @@
+window["channel: " + game.id ] = pusher.subscribe("game_channel_number_" + game.id );
+
+window["channel: " + game.id ].bind('game_data_for_' +  game.id , function(data) {
+	if (data.message[0] == "t"){    //a turn ended
+		location.reload();
+	}
+	if (data.message[0] == "c"){    //cards finished dealing
+		location.reload();	
+	}
+	if (((JSON.parse(data.message)).action === "coup") && ((JSON.parse(data.message)).opponent == current_user.nickname)){
+		 document.getElementById('reactToCoup').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "assassin") && ((JSON.parse(data.message)).opponent == current_user.nickname)){
+		 document.getElementById('reactToAssassin').className = ""
+		 document.getElementById('claimContessa').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "theft") && ((JSON.parse(data.message)).opponent == current_user.nickname)){
+		document.getElementById('reactToTheft').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "foreign aid") && ((JSON.parse(data.message)).opponent != current_user.nickname)){
+		 document.getElementById('reactToForeignAid').className = ""
+	}
+	if (((JSON.parse(data.message)).result === "fail") && ((JSON.parse(data.message)).player === current_user.nickname)){
+		document.getElementById('reactToFailedChallenge').className = "";
+		document.getElementById('reactToTax').className = "hidden";
+		document.getElementById('reactToTheft').className = "hidden";
+		document.getElementById('reactToExchange').className = "hidden";
+		document.getElementById('reactToBlockTheftCaptin').className = "hidden";
+		document.getElementById('reactToBlockTheftAmbassador').className = "hidden";
+		document.getElementById('reactToBlockContessa').className = "hidden";
+		document.getElementById('reactToForeignAid').className = "hidden";
+	}
+	if (((JSON.parse(data.message)).result === "succeede") && ((JSON.parse(data.message)).player === current_user.nickname )){
+		document.getElementById('reactToSuccessfulChallenge').className = "";
+	}
+	if (((JSON.parse(data.message)).action === "tax") && ((JSON.parse(data.message)).opponent != current_user.nickname)){
+		 document.getElementById('reactToTax').className = ""
+	}
+	if ((JSON.parse(data.message)).action === "block"){
+		var infoString = (JSON.parse(data.message)).opponent + " blocks with " + (JSON.parse(data.message)).card
+		document.getElementById('displayBlockInfo').innerHTML = infoString
+
+	}
+	if (((JSON.parse(data.message)).action === "block") && ((JSON.parse(data.message)).opponent != current_user.nickname) &&  (JSON.parse(data.message)).card == 'Contessa'){
+		 document.getElementById('reactToBlockContessa').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "block") && ((JSON.parse(data.message)).opponent != current_user.nickname) &&  (JSON.parse(data.message)).card == 'Duke'){
+		 document.getElementById('reactToBlockForeignAid').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "block") && ((JSON.parse(data.message)).opponent != current_user.nickname) &&  (JSON.parse(data.message)).card == 'Captin'){
+		 document.getElementById('reactToBlockTheftCaptin').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "block") && ((JSON.parse(data.message)).opponent != current_user.nickname) &&  (JSON.parse(data.message)).card == 'Ambassador'){
+		 document.getElementById('reactToBlockTheftAmbassador').className = ""
+	}
+	if (((JSON.parse(data.message)).action === "exchange") && ((JSON.parse(data.message)).opponent != current_user.nickname)){
+		 document.getElementById('reactToExchange').className = ""
+	}
+
+	console.log(data.message)
+
+})
